50a9081738687e052009a730c124a49e8b9c1f6e
Victor Marchesi <victor_marchesi@icloud.com>
2025-02-03 02:47:51 -0300

    criando modelo e migracoes

---
+++ comandos/README.md
@@ -0,0 +1,9 @@
+Criando o ambiente virtual e instação do Django
+
+python -m venv venv
+venv/Scripts/activate
+pip install django 
+django-admin startproject myproject
+python manage.py startapp products
+
+Criação do models e migrações
--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    
+
+    def __str__(self):
+        return self.nome
